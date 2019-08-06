fe71bec4d92851d73b5016b50ccb653fde43c378
Vladislav Trotsenko <admin@bestweb.com.ua>
2019-08-06 18:09:09 +0300

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,4 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
-
 end
