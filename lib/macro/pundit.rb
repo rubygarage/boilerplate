d2cc224de81552bab112da5887f8bfb688b6589d
alexyndr belik <alexyndr@gmail.com>
2020-11-12 17:08:55 +0200

    Fix rubocop

--- lib/macro/pundit.rb
+++ lib/macro/pundit.rb
@@ -1,36 +1,39 @@
 # frozen_string_literal: true
 
-module Policy
-  def self.Pundit(policy_class, user: nil, model: nil, action, name: :default)
-    Policy.step(Pundit.build(policy_class, user, model, action), name: name)
-  end
-
-  module Pundit
-    def self.build(*args, &block)
-      Condition.new(*args, &block)
+module Macro
+  module Policy
+    def self.Pundit(policy_class, action, user: nil, model: nil, name: :default)
+      Policy.step(Pundit.build(policy_class, action, user: user, model: model), name: name)
     end
 
-    class Condition
-      def initialize(policy_class, user, model, action)
-        @policy_class, @user, @model, @action = policy_class, user, model, action
+    module Pundit
+      def self.build(*args, &block)
+        Condition.new(*args, &block)
       end
 
-      def call((options), *)
-        policy = build_policy
-        result!(policy.send(@action), policy)
-      end
+      class Condition
+        def initialize(policy_class, action, user, model)
+          @policy_class, @user, @model, @action = policy_class, user, model, action
+        end
 
-    private
-      def build_policy
-        @policy_class.new(@user, @model) if @user && @model
-        @policy_class.new(options[:current_user], options[:model]) unless @user && @model
-      end
+        def call((options), *)
+          policy = build_policy(options)
+          result!(policy.send(@action), policy)
+        end
 
-      def result!(success, policy)
-        data = { policy: policy }
-        data[:message] = "Breach" if !success
+        private
 
-        Trailblazer::Operation::Result.new(success, data)
+        def build_policy(options)
+          @policy_class.new(@user, @model) if @user && @model
+          @policy_class.new(options[:current_user], options[:model]) unless @user && @model
+        end
+
+        def result!(success, policy)
+          data = { policy: policy }
+          data[:message] = 'Breach' unless success
+
+          Trailblazer::Operation::Result.new(success, data)
+        end
       end
     end
   end
