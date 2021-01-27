139e87b39fd9f14107de35af4497a5c227ffd126
WoLkErSs <ivanpauls1705@gmail.com>
2021-01-27 10:07:48 +0200

    Allowed use Macro::ModelRemove several times

--- lib/macro/model_remove.rb
+++ lib/macro/model_remove.rb
@@ -4,11 +4,12 @@ module Macro
   def self.ModelRemove(path: [], type: :destroy)
     task = Trailblazer::Activity::TaskBuilder::Binary(
       ->(ctx, **) {
-        model = ctx[path.shift]
+        model = ctx[path.first]
         types = %i[destroy delete destroy! delete!]
+        call_chain = path[1..]
         return unless types.include?(type)
 
-        path.empty? ? model.public_send(type) : path.push(type).inject(model, :try)
+        call_chain.empty? ? model.public_send(type) : call_chain.push(type).inject(model, :try)
       }
     )
     { task: task, id: "#{name}/#{__method__}_id_#{task.object_id}".underscore }
