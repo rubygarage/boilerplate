70c2fa3b0d7711d3630e921858f4348d4244390b
Ivan Pauls <37914720+WoLkErSs@users.noreply.github.com>
2021-02-08 11:16:13 +0200

    [ISSUE-315] | Make UUID as primary key by default (#316)
    * Replaced id with uuid
    
    * Unite an migration

--- db/migrate/20190813110527_create_users.rb
+++ db/migrate/20190813110527_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
-      t.belongs_to :account, index: true
+    create_table :users, id: :uuid do |t|
+      t.uuid :account_id, index: true
       t.string :name
 
       t.timestamps
--- db/migrate/20190827053243_add_verification_to_accounts.rb
+++
@@ -1,9 +0,0 @@
-class AddVerificationToAccounts < ActiveRecord::Migration[6.0]
-  def up
-    add_column :accounts, :verified, :boolean, default: false
-  end
-
-  def down
-    remove_column :accounts, :verified
-  end
-end
