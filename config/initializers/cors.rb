abc583aeb2a7ece4f7cf099e8b7c439db3a35d6c
Alexander Katulskiy <alexxty7@gmail.com>
2022-03-11 13:41:32 +0200

    WIP

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,13 +6,14 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
+ALLOWED_METHODS = %i[get post put patch delete options head].freeze
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'localhost:3000'
-
-    resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head]
+    origins(
+      /\Ahttp:\/\/localhost(:\d+)?\z/,
+      /\Ahttp:\/\/127\.0\.0\.1(:\d+)?\z/
+    )
+    resource('*', headers: :any, methods: ALLOWED_METHODS, expose: ['Content-Disposition'])
   end
 end
